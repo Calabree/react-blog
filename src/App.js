53d3bb3e7b59b8855333b51e3c683f2c6f1463e7
Peter Calabrese <peter@calabrese.tech>
2022-05-19 16:59:56 -0400

    root file w/ react route

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BlogDetails from "./BlogDetails";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
